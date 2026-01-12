d23138528cc8ddbc0b06f027bf274ee612bba6ce
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-12 18:42:18 +0200

    Aprendendo dicionarios

---
+++ exercicios.py
@@ -0,0 +1,18 @@
+
+""""import math
+
+angulo = float(input("Angulo qualquer: "))
+print(f"Seno: {math.sin(angulo)}\nCo-seno: {math.cos(angulo)}\nTangente: {math.tan(angulo)}")
+"""
+
+import random
+aluno = []
+
+for x in range(4):
+    aluno.append(input("Aluno: "))
+aluno.sort()
+
+print(aluno)
+
+
+
