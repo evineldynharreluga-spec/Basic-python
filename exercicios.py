852ec2a3a47fa1569f6edcb2ff8e0c5c151be8ca
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-15 11:31:48 +0200

    Updates de ontem

--- exercicios.py
+++ exercicios.py
@@ -1,24 +1,7 @@
 
-<<<<<<< HEAD
-
-print("Starting Git!")
-print("Hello!")
-=======
-""""import math
-
-angulo = float(input("Angulo qualquer: "))
-print(f"Seno: {math.sin(angulo)}\nCo-seno: {math.cos(angulo)}\nTangente: {math.tan(angulo)}")
-"""
-
-import random
-aluno = []
-
-for x in range(4):
-    aluno.append(input("Aluno: "))
-aluno.sort()
-
-print(aluno)
-
-
->>>>>>> d23138528cc8ddbc0b06f027bf274ee612bba6ce
-
+#Ex. 1
+first_name = "Evineldy"
+last_name = "Nharreluga" 
+full_name = "Evineldy Antonio Nharreluga"
+country = "Mozambique"
+city = "Maputo"
