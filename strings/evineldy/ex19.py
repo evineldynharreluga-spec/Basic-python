f1f07180fb2538433743d0f320de9c6b0499e669
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-19 17:10:10 +0200

    solved exercise 19

---
+++ strings/evineldy/ex19.py
@@ -0,0 +1,7 @@
+string = 'Coding For All'
+split_string = string.split(' ')
+print(split_string)
+splited_pos1 = split_string[0]
+splited_pos2 = split_string[1]
+splited_pos3 = split_string[2]
+print(splited_pos1[0] + '' + splited_pos2[0] + '' + splited_pos3[0])
