13920c7fcec3aa074ee2a7a4e99a60ab3800a35b
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-19 17:06:42 +0200

    solved exercise 18

---
+++ strings/evineldy/ex18.py
@@ -0,0 +1,6 @@
+string = "Python For Everyone"
+string_splited = string.split(" ")
+pos1 = string_splited[0]
+pos2 = string_splited[1]
+pos3 = string_splited[2]
+print(pos1[0] +''+pos2[0]+''+pos3[0])
