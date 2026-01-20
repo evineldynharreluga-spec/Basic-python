4b4442bfafa23aa535321c6bd26722ad8b645cd3
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-20 16:18:18 +0200

    solved exercise 4

---
+++ lists/evineldy/ex04.py
@@ -0,0 +1,4 @@
+lista = ['Maputo', 'Gaza', 'Inhambane', 'Nampula', 'Sofala']
+print(f'First item: {lista[0]}')
+print(f'Second item: {lista[len(lista)-3]}')
+print(f'Last item: {lista[len(lista)-1]}')
