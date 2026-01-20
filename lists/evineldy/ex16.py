651e552e186fe4617bd55eda6d312fe1884bd0fd
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-20 21:23:25 +0200

    solved exercise 18

---
+++ lists/evineldy/ex16.py
@@ -0,0 +1,5 @@
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+it_companies.sort()
+print(it_companies)
+it_companies.sort(reverse=True)
+print(it_companies)
