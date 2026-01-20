79399f6c6be5754979d5ede0579bb71deaa03e08
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-20 22:12:12 +0200

    solved exercise 27

---
+++ lists/evineldy/ex27.py
@@ -0,0 +1,9 @@
+
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+front_end.extend(back_end)
+print(front_end)
+full_stack = front_end.copy()
+added_list = 'Python', 'SQL'
+full_stack.insert(-3, 'Python')
+print(full_stack)
