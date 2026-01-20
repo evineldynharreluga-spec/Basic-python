0ccd06c350abf8f7104f260f375c683c5ffe41e6
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-20 16:50:10 +0200

    solved exercise 13

---
+++ lists/evineldy/ex13.py
@@ -0,0 +1,6 @@
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+company_upper = it_companies[3].upper()
+it_companies.insert(3, company_upper)
+#print(it_companies)
+it_companies.remove('Apple')
+print(it_companies)
