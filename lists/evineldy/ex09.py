58227e10a79c914f24adc7702f6746e868d894f0
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-20 16:35:11 +0200

    solved exercise 9

---
+++ lists/evineldy/ex09.py
@@ -0,0 +1,4 @@
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+print(f'First company: {it_companies[0]}')
+print(f'Middle company: {it_companies[3]}')
+print(f'Last company: {it_companies[len(it_companies)-1]}')
