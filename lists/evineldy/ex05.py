f109d891b251df243a0f15d51294f9acabffb207
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-20 16:22:10 +0200

    solved exercise 5

---
+++ lists/evineldy/ex05.py
@@ -0,0 +1,4 @@
+mixed_data_types = [{'Name': 'Evineldy', 'Age': 21, 'Height': 1.75, 'Marital status': 'Single', 'Address': 'Hulene B'}]
+
+#or
+mixed_data_types = ['Evineldy', 21, 1.75, 'Single', 'Hulene B']
