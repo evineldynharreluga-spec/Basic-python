4fb128f8b04f2e8fc02cb2c54d83472073448b25
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-22 12:43:52 +0200

    solved exercise 5 of level 1

---
+++ sets/evineldy/ex05-level1.py
@@ -0,0 +1,7 @@
+'''
+The difference between remove and discard, is that
+* remove: raise an error if trying to remove an item that
+          doesnt exist
+* discard: doesnt raise errors even if the item doesnt exist
+
+'''
