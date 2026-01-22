59640378edf0bce390ab3399f5c5565cf3930d65
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-22 13:00:07 +0200

    solved exercise 1 of level 3

---
+++ sets/evineldy/ex01-level3.py
@@ -0,0 +1,10 @@
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+print(age)
+age_set = set(age)
+print(age_set)
+print(len(age) == len(age_set))
