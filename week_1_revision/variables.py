f3ba9855740c132ef72c097bff460ca8dd6c3330
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-25 13:24:20 +0200

    solved variables exercises as revision

---
+++ week_1_revision/variables.py
@@ -0,0 +1,28 @@
+#Day 2: 30 Days of python programming
+
+first_name = 'Evineldy'
+last_name = 'Nharreluga'
+full_name = 'Evineldy Nharreluga'
+country = 'Mozambique'
+city = 'Maputo'
+age = 21
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+
+name, second_name = 'Evineldy', 'Nharreluga'
+
+''''print(name)
+print(second_name) ''' 
+
+print(type(first_name))
+print(len(first_name))
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+remainder = num_one % num_two
+floor_division = num_one // num_two
