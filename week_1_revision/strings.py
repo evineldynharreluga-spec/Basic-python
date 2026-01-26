66ae2e99106e0544c207fa37ad1fbfc8c0148acd
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-26 11:32:02 +0200

    solved strings exercises for revision

---
+++ week_1_revision/strings.py
@@ -0,0 +1,44 @@
+''''string = 'Thirty', 'Days', 'Of', 'Python'
+result = ' '.join(string)
+print(result)'''
+
+#string = 'Coding For All'
+''''print(string[0])
+print(string.find('Coding'))
+print(string.split(" "))'''
+
+name = 'Coding For All People'
+result = name.split()
+name1 = result[0]
+name2 = result[1]
+name3 = result[2]
+
+#print(name1[0]+name2[0]+name3[0])
+
+#print(name.index('F'))
+#print(name.rindex('l'))
+
+frase = 'You cannot end a sentence with because because because is a conjunction'
+''''print(frase.find('because'))
+print(frase.rindex('because'))
+print(frase[31:55])'''
+
+string = 'Coding For All'
+#print(string.startswith('Coding'))
+#print(string.endswith('coding'))
+
+languages = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+joined = '# '.join(languages)
+#print(joined)
+
+titles = 'Name\tAge\tCountry\tCity'
+subtitles = 'Asabeneh\t250\tFinland\tHelsinki'
+
+''''print(titles.expandtabs(10))
+print(subtitles.expandtabs(10))'''
+
+
+print(f'8 + 6 = {8 + 6}')
+print(f'8 - 6 = {8 - 6}')
+
+
