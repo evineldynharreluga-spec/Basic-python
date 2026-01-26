60a92c41169dcedcd8d482ef087bb7f839e66f79
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-26 16:52:19 +0200

    solved exercise 2 of level 2

---
+++ conditionals/ex02-level2.py
@@ -0,0 +1,9 @@
+month = input('Enter month you are in now: ')
+if month in ('September', 'October', 'November'):
+    print('AUTUMN!')
+elif month in ('December', 'January', 'February'):
+    print('WINTER!')
+elif month in ('March', 'April', 'May'):
+    print('SPRING!')
+else:
+    print('SUMMER!!')
