4a401f15fb3471482ae0c95e155743108882cdcf
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-01-29 10:32:20 +0200

    solved exercise 2 of level 3

---
+++ loops/ex02-level3.py
@@ -0,0 +1,10 @@
+fruits = ['banana', 'orange', 'mango', 'lemon']
+#             0          1        2         3
+#             3          2        1         0
+#
+
+for fruit in reversed(fruits):
+    print(fruit)
+
+
+
