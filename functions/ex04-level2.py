2d0bc73346ce2836cc8a9ea7be8a731f3207ca90
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-02-02 13:23:01 +0200

    solved exercise 4 of level 2

--- functions/ex04-level2.py
+++ functions/ex04-level2.py
@@ -6,3 +6,15 @@ def calculate_mean(*lst):
     media = soma / len(lst)
     return media
 print('Media: ', calculate_mean(1, 2, 3, 5))
+
+def calculate_median(*lst):
+    tamanho = int(len(lst) / 2)
+    if len(lst)-1 % 2 == 0:
+        elemento = lst[tamanho]
+        print('Mediana e: ',elemento)
+    else:
+        elemento_1 = lst[tamanho-1]
+        elemento_2 = lst[tamanho]
+        mediana = (elemento_2 + elemento_1)/2
+        print('Mediana e ',mediana)
+print(calculate_median(1, 3, 5, 7, 9, 11, 13))
