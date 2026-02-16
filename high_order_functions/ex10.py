d1c1f0413d32ec6c2b55c754d6af8622d31a963c
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-02-16 18:23:40 +0200

    solved exercise 10

---
+++ high_order_functions/ex10.py
@@ -0,0 +1,8 @@
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+from functools import reduce
+
+sum_all = reduce(lambda x, y: x + y, numbers)
+print(sum_all)
