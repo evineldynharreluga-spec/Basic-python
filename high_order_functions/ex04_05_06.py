870e4bceca68e7cb91ff015c19e352d19737fd74
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-02-16 17:45:40 +0200

    solved exercises 4, 5, 5

---
+++ high_order_functions/ex04_05_06.py
@@ -0,0 +1,13 @@
+countries = ['Estonia', 'Finland', 'Sweden', 
+             'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for country in countries:
+    print(country)
+
+for name in names:
+    print(name)
+
+for num in numbers:
+    print(num)
