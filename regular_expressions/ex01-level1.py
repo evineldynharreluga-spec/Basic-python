d546f3565e069123d9866383077033564319437d
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-02-18 14:55:08 +0200

    solved exercise 1 of level 1

---
+++ regular_expressions/ex01-level1.py
@@ -0,0 +1,13 @@
+paragraph = 'I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.'
+
+import re
+most_frequent_word = re.findall('i', paragraph, re.I)
+print(len(most_frequent_word))
+
+separado = paragraph.split()
+print(separado)
+
+
+for word in separado:
+    print(f'{word}', len(re.findall(word, paragraph)))
+
