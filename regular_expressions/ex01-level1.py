09ae4af257b3d23215e7535b6aa9f6b0155e37b7
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-02-19 10:09:23 +0200

    created folder file_handling

--- regular_expressions/ex01-level1.py
+++ regular_expressions/ex01-level1.py
@@ -1,13 +1,16 @@
 paragraph = 'I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.'
 
 import re
-most_frequent_word = re.findall('i', paragraph, re.I)
+''''most_frequent_word = re.findall('i', paragraph, re.I)
 print(len(most_frequent_word))
-
+'''
 separado = paragraph.split()
-print(separado)
+#print(separado)
 
 
-for word in separado:
+''''for word in separado:
     print(f'{word}', len(re.findall(word, paragraph)))
+'''
 
+lista = [(word, len(re.findall(word, paragraph))) for word in separado]
+print(lista)
---
+++ regular_expressions/ex01-level3.py
@@ -0,0 +1,10 @@
+import re
+
+sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
+
+regex_pattern = r'^$'
+replace = re.findall(regex_pattern, sentence)
+print(replace)
+findings = re.sub('%|&|@|$|#|;|!', '', sentence)
+
+print(findings)
