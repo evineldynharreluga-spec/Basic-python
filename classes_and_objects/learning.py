9b8aa868a9aa8197a52eede712f5798664f60b12
Evineldy Nharreluga <evineldy.nharreluga@gmail.com>
2026-03-03 11:31:00 +0200

    added a test-application: Cars Management System

--- classes_and_objects/learning.py
+++ classes_and_objects/learning.py
@@ -116,3 +116,91 @@ s1.add_skills('JavaScript')
 print(s1.skills)
 
 
+
+
+class Carro:
+    
+    def __init__(self, marca, modelo, preco):
+        self.marca = marca
+        self.modelo = modelo
+        self.preco = preco
+        self.pecas = []
+    def criarCarro(self):
+        return f'Marca: {self.marca}\nModelo: {self.modelo}\nPreco: {self.preco}'
+    def adicionarPecas(self, peca):
+        self.pecas.append(peca)
+        
+
+c1 = Carro('Mercedes', 'Benz', 2000000)
+print(c1.criarCarro())
+
+
+c1.adicionarPecas('janta')
+c1.adicionarPecas('pneu')
+c1.adicionarPecas('vidros fumados')
+print(c1.pecas)
+
+class Viatura(Carro):
+
+    def __init__(self, marca, modelo, preco, importado):
+        self.importado = importado
+        super().__init__(marca, modelo, preco)
+    def criarCarro(self):
+        importado = 'Sim' if self.importado=='S' else 'Nao'
+        return f'Marca: {self.marca}\nModelo: {self.modelo}\nPreco: {self.preco}\nImportado: {importado}'
+
+v1 = Viatura('BMW', 'x6', 30000000, 'S')
+v1.adicionarPecas('pneu')
+v1.adicionarPecas('jantes')
+print(v1.criarCarro())
+print(v1.pecas)
+
+
+print('===SISTEMA DE GESTAO DE VIATURAS====')
+print('===MENU===')
+print('[1] Adicionar viatura')
+print('[2] Remover viaturas')
+print('[3] Ver viaturas adicionadas')
+opcao = input('Escolha uma opcao: ')
+
+if opcao == '1':
+    #Adicionar viatura
+    print('\n=======REGISTRO DE VIATURAS========')
+    marca = input('Introduza a marca do carro: ')
+    modelo = input('Introduza o modelo: ')
+
+    #Adicionar preco
+    while True:
+        try:
+            preco = float(input('Introduza o preco: '))
+            break   
+        except ValueError:
+            print('Preco invalido. Introduza novamente.')
+            
+
+    #Importado ou nao
+    while True:
+        importado = input('O carro e importado? [S/N]').upper()
+        if importado in ('S', 'N'):
+            break
+        print('Resposta invalida!')
+    v2 = Viatura(marca, modelo, preco, importado)
+
+    # Adicionar pecas
+    print('\n====ADICIONAR PECAS====')
+    while True:
+        peca = input('Adicionar peca (ou ENTER para nao introduzir): ')
+        if peca == '':
+            break
+        v2.adicionarPecas(peca)
+print('VIATURA INTRODUZIDA!')
+
+    
+''''if opcao == '3':
+    # Dados da Viatura'''
+print('\n===DADOS DA VIATURA===')
+print(v2.criarCarro())
+print('Pecas: ', v2.pecas)
+ 
+        
+   
